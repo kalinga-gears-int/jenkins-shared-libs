8abb3140aeb233ab3bad063c01f365abdcd59bed
kalinga-gears-int <92032532+kalinga-gears-int@users.noreply.github.com>
2021-10-26 19:20:00 +0530

    Update angularSonarScan.groovy

--- vars/angularSonarScan.groovy
+++ vars/angularSonarScan.groovy
@@ -5,7 +5,7 @@ def call(Map config=[:]){
         def scannerHome = tool 'Dev Sonar Scanner';
         withSonarQubeEnv('Dev SonarQube Server') {
         sh "${scannerHome}/bin/sonar-scanner \
-        -Dsonar.projectKey=config.project \
+        -Dsonar.projectKey=${config.project} \
         -Dsonar.sources=. \
         -Dsonar.host.url=http://10.0.0.11:9009 \
         -Dsonar.login=1e8ef92786cab5caf31c1ecce580bc9b24e2cd4f"
@@ -13,7 +13,7 @@ def call(Map config=[:]){
         
     }
     
-        
+    echo "project is ${config.project}"
     def qg = waitForQualityGate();
         if(qg.status != "OK"){
             echo "Quality Gate Not OK"
