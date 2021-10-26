9eac7514fac9c31cf3f000fdafa417c238f7f386
kalinga-gears-int <92032532+kalinga-gears-int@users.noreply.github.com>
2021-10-26 20:55:58 +0530

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,20 @@
+def call(project){    
+
+
+    timeout(time: 1, unit: 'HOURS') {
+        def scannerHome = tool 'Dev Sonar Scanner';
+        withSonarQubeEnv('Dev SonarQube Server') {
+        sh "${scannerHome}/bin/sonar-scanner \
+        -Dsonar.projectKey=${project} \
+        -Dsonar.sources=. \
+        -Dsonar.host.url=http://10.0.0.11:9009 \
+        -Dsonar.login=1e8ef92786cab5caf31c1ecce580bc9b24e2cd4f"
+    };
+        
+    }
+
+    def qg = waitForQualityGate();
+    
+    echo ${pg}
+        
+}
