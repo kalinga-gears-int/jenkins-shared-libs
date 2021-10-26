fbf2419b0123944672d4e9ce0fbe6fa5f0fc5efc
kalinga-gears-int <92032532+kalinga-gears-int@users.noreply.github.com>
2021-10-26 15:12:40 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -4,7 +4,9 @@ def call(String name = 'User') {
 
 
 def sonarScan(Map config=[:]){
+    
     def scannerHome = tool 'Dev Sonar Scanner';
+    
     withSonarQubeEnv('Dev SonarQube Server') { // If you have configured more than one global server connection, you can specify its name
     sh "${scannerHome}/bin/sonar-scanner \
     -Dsonar.projectKey=${config.project} \
@@ -12,6 +14,4 @@ def sonarScan(Map config=[:]){
     -Dsonar.host.url=http://10.0.0.11:9009 \
     -Dsonar.login=${config.key}"
     }
-    def scanID = waitForQualityGate();
-	echo "ScanID is ${scanID}"
 }
