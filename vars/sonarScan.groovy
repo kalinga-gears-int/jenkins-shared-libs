3f69f52e594cec28575e67a6c95168fe1edbf194
kalinga-gears-int <92032532+kalinga-gears-int@users.noreply.github.com>
2021-10-26 16:39:12 +0530

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,10 +1,10 @@
 def call(Map config=[:]){    
     def scannerHome = tool 'Dev Sonar Scanner';
     withSonarQubeEnv('Dev SonarQube Server') {
-        sh"${scannerHome}/bin/sonar-scanner \
-  -Dsonar.projectKey=MOE-Frontend \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://10.0.0.11:9009 \
-  -Dsonar.login=1e8ef92786cab5caf31c1ecce580bc9b24e2cd4f"
+        sh "${scannerHome}/bin/sonar-scanner \
+                            -Dsonar.projectKey=MOE-Frontend \
+                            -Dsonar.sources=. \
+                            -Dsonar.host.url=http://10.0.0.11:9009 \
+                            -Dsonar.login=1e8ef92786cab5caf31c1ecce580bc9b24e2cd4f"
     };
 }
