bb597988f2e1c1350bffeade24ea7aeb3e0782e8
kalinga-gears-int <92032532+kalinga-gears-int@users.noreply.github.com>
2021-10-28 12:13:34 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,3 +1,3 @@
 def call(checkout_status){    
-    emailext attachLog: true, body: "Checkout: ${checkout_status}", subject: "${PROJECT_NAME} - BuildID # ${BUILD_NUMBER} - ${BUILD_STATUS}!", to: "kalinga.ranasinghe@gears-int.com"        
+    emailext attachLog: true, body: "Checkout: ${checkout_status}", subject: "", to: "kalinga.ranasinghe@gears-int.com"        
 }
